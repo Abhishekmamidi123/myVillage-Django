07cd8b5a84f94ce645003bf3121d837fa103708c
Abhishekmamidi <abhishek.m15@iiits.in>
2017-09-16 07:07:08 +0530

    Add files via upload

--- myVillage/myVillage/settings.py
+++ myVillage/myVillage/settings.py
@@ -141,3 +141,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
