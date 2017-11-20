62a45761336e2cb9ac8c598fcb87f4a09071082b
vagdevik <30801221+vagdevik@users.noreply.github.com>
2017-11-20 12:20:48 +0000

    Predict - graph added

--- myVillage/predict/views.py
+++ myVillage/predict/views.py
@@ -27,13 +27,25 @@ def weather(request):
     	temp = ''
     	for x in temperature:
 			temp+=str(x)+' '
-    predicted_value = os.popen('python holtWinter.py "' + temp  + '"').read()[:-1]
+    predicted_value = os.popen('python predict/holtWinter.py "' + temp  + '"').read()[:-1]
+    print predicted_value
     tomorrow = str(datetime.datetime.strptime(dates[len(dates)-1][2:], '%y-%m-%d').date() + datetime.timedelta(days=1))
     dates.append(tomorrow)
-    temperature.append(predicted_value)
-    print dates
-    print temperature
-    return render(request, "predict/weather.html", {'dates':dates, 'temperature':temperature})
+    temperature.append(round(float(predicted_value),2))
+    print len(dates)
+    print len(temperature)
+    data = []
+    for i in range(len(dates)):
+		temp = {}
+		temp['date'] = dates[i]
+		temp['value'] = temperature[i]
+		if i==len(dates)-2:
+			temp['dashLengthLine'] = 5
+		if i==len(dates)-1:
+			temp['dashLengthLine'] = 5
+			temp['additional'] = '(Predicted)'				
+		data.append(temp)
+    return render(request, "predict/weather.html", {'data':json.dumps(data)})
 
 def market(request):
 	return HttpResponse("hello")
